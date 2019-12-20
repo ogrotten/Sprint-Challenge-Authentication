72ef4c75080dddb144638cb12100d3ee96f64ce8
Darrin Lowery <lambda@monsto.com>
2019-12-20 11:02:39 -0600

    login works.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,11 +4,13 @@ const jwt = require("jsonwebtoken"); // installed this
 
 const Users = require("../users/users-model.js");
 
-function clg(...x) {console.log(...x)}
+function clg(...x) { console.log(...x) }
 
 router.post('/register', (req, res) => {
+	// implement registration
+
 	let user = req.body;
-	clg("11", user);
+	clg("13", user);
 	const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
 	user.password = hash;
 
@@ -23,6 +25,42 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
 	// implement login
+
+	let { username, password } = req.body;
+	Users.findBy({ username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				// sign token
+				const token = signToken(user); // new line
+
+				// send the token
+				res.status(200).json({
+					token, // added token as part of the response sent
+					message: `Welcome ${user.username}!`,
+				});
+			} else {
+				res.status(401).json({ message: "Invalid Credentials" });
+			}
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
 });
+
+function signToken(user) {
+	const payload = {
+		username: user.username,
+	};
+
+	const secret = process.env.JWT_SECRET || "do you liike jokes?";
+
+	const options = {
+		expiresIn: "1m",
+	};
+
+	return jwt.sign(payload, secret, options); // notice the return
+}
+
 
 module.exports = router;
